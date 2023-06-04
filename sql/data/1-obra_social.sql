23f62d27c937ae0816fccd3a2423315c4b662fd2
Francisco Ruiz <francisco.ruiz@mercadolibre.com>
2023-06-04 12:25:23 -0300

    feat: added insert data of obra_social

---
+++ sql/data/1-obra_social.sql
@@ -0,0 +1,6 @@
+insert into obra_social (nro_obra_social, nombre, contacto_nombre, contacto_apellido, contacto_telefono, contacto_email) values (1, 'Hospira, Inc.', 'Bordie', 'Traylen', '1712381922', 'btraylen0@rediff.com');
+insert into obra_social (nro_obra_social, nombre, contacto_nombre, contacto_apellido, contacto_telefono, contacto_email) values (2, 'Publix Super Markets Inc', 'Constantino', 'Ray', '7414670950', 'cray1@goo.gl');
+insert into obra_social (nro_obra_social, nombre, contacto_nombre, contacto_apellido, contacto_telefono, contacto_email) values (3, 'Topco Associates, LLC', 'Felita', 'Ormiston', '9231257391', 'formiston2@cafepress.com');
+insert into obra_social (nro_obra_social, nombre, contacto_nombre, contacto_apellido, contacto_telefono, contacto_email) values (4, 'Landy International', 'Klement', 'Hawick', '7666585861', 'khawick3@istockphoto.com');
+insert into obra_social (nro_obra_social, nombre, contacto_nombre, contacto_apellido, contacto_telefono, contacto_email) values (5, 'Amerisource Bergen', 'Luther', 'Wilce', '7998941232', 'lwilce4@dropbox.com');
+insert into obra_social (nro_obra_social, nombre, contacto_nombre, contacto_apellido, contacto_telefono, contacto_email) values (6, 'Stratus Pharamceuticals', 'Corbin', 'Antognelli', '2721589698', 'cantognelli5@apache.org');
