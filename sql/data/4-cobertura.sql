8fdfe09bbafea3669c10a83509824b7c2859ad55
agustinesco <agustin.escobar@lambdaclass.com>
2023-06-04 12:41:26 -0300

    feat: Add coverage insertion

---
+++ sql/data/4-cobertura.sql
@@ -0,0 +1,24 @@
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (32145789, 4, 4444.12, 2222.40);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (24587963, 4, 71957.65, 53756.52);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (38692417, 4, 14135.81, 34241.4);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (41256890, 4, 84766.27, 37199.65);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (29784562, 4, 17978.8, 93422.7);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (37894125, 4, 90085.54, 66818.65);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (45621398, 4, 52150.02, 93644.91);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (33964752, 4, 35126.41, 63643.75);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (26387951, 4, 21734.21, 33293.65);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (31659274, 4, 31110.06, 39263.85);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (36725198, 4, 84322.42, 87006.57);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (42891753, 4, 65594.66, 61862.61);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (27536914, 4, 83912.46, 69885.92);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (35198472, 4, 33872.46, 43888.66);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (43961825, 4, 54695.1, 87017.68);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (30874512, 4, 52565.89, 26753.06);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (28319476, 4, 59673.19, 80314.05);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (34765129, 4, 35920.65, 93989.1);
+
+-- medics with multiples coverage
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (40271684, 4, 74805.3, 72455.77);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (40271684, 8, 97854.33, 95381.25);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (25648973, 4, 17558.8, 45408.05);
+insert into cobertura (dni_medique, nro_obra_social, monto_paciente, monto_obra_social) values (25648973, 12, 36947.6, 16390.02);
