63540a0efd04e3dd922696973651d5cf7b1c1164
Francisco Ruiz <francisco.ruiz@mercadolibre.com>
2023-06-05 19:14:19 -0300

    fix: add filter by id and change email_body in send_email_on_appointment_canceled trigger

--- sql/triggers/send_email_on_appointment_canceled.sql
+++ sql/triggers/send_email_on_appointment_canceled.sql
@@ -1,13 +1,17 @@
 create or replace function send_email_on_appointment_canceled() returns trigger as $$
 declare
     result record;
+    last_id integer;
+    email_id integer;
+    email_title varchar;
     email_body varchar;
+    email_status varchar;
 begin
-    if new.estado == old.estado and new.estado == 'cancelado' then
+    if new.estado = old.estado then
         return new;
     end if;
 
-    if new.estado != 'cancelado' then
+    if new.estado <> 'cancelado' then
         return new;
     end if;
 
@@ -15,16 +19,27 @@ begin
         t.nro_turno,
         concat(p.nombre, ' ', p.apellido) as nombre_paciente,
         p.email as email_paciente,
-        concat(m.nombre, ' ', m.apellido) as nombre_medique
+        concat(m.nombre, ' ', m.apellido) as nombre_medique,
+        t.fecha as fecha_turno
     into result
     from turno t, paciente p, medique m
     where t.nro_paciente = p.nro_paciente
-    and m.dni_medique = t.dni_medique;
+    and m.dni_medique = t.dni_medique
+    and t.nro_turno = old.nro_turno;
 
-    email_body := concat('Hola, ',  result.nombre_paciente,'! Su turno nro', result.nro_turno, 'ha sido cancelado con el medico ', result.nombre_medique);
+    select max(nro_email) into last_id from envio_email;
 
-    insert into envio_email (f_generacion, email_paciente, asunto, cuerpo, f_envio, estado)
-    values (now(), result.email_paciente, 'Cancelación de turno', email_body, now(), 'enviado');
+    if last_id is null then
+        last_id = 0;
+    end if;
+
+    email_id := last_id + 1;
+    email_title := 'Cancelación de turno';
+    email_body := concat('¡Hola, ', result.nombre_paciente,'! Su turno con el medico ', result.nombre_medique, ' del día ', result.fecha_turno , ' ha sido cancelado. Pronto el centro de atención se contactará con usted.');
+    email_status := 'enviado';
+
+    insert into envio_email (nro_email, f_generacion, email_paciente, asunto, cuerpo, f_envio, estado)
+    values (email_id, now(), result.email_paciente, email_title, email_body, now(), email_status);
 
     return new;
 end;
