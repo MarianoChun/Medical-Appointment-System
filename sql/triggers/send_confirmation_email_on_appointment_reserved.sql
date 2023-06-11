21edbbb2de35cc8a7e9e0efa637d89a838504351
agustinesco <agustinesco@outlook.es>
2023-06-11 16:55:14 -0300

    Move email creation to stored procedure

--- sql/triggers/send_confirmation_email_on_appointment_reserved.sql
+++ sql/triggers/send_confirmation_email_on_appointment_reserved.sql
@@ -5,6 +5,7 @@ declare
     medic record;
     title text := 'Reserva de turno';
     body text;
+    estado text := 'pendiente';
 begin
     if new.estado = old.estado then
         return new;
@@ -17,9 +18,10 @@ begin
     select * from paciente where nro_paciente = new.nro_paciente into patient;
     select * from medique where dni_medique = new.dni_medique into medic;
 
-    select format('Turno reservado para el paciente %s, %s en la fecha de %s a las %s en el consultorio numero %s con el medico %s, %s' ,patient.apellido, patient.nombre, new.fecha::date, new.fecha::time, new.nro_consultorio, medic.apellido, medic.nombre) into body;
+    select format('Turno reservado para el paciente %s, %s en la fecha de %s a las %s en el consultorio numero %s con el medico %s, %s', patient.apellido, patient.nombre, new.fecha::date, new.fecha::time, new.nro_consultorio, medic.apellido, medic.nombre) into body;
 
-    select send_email(patient.email, title, body);
+    insert into envio_email (f_generacion, email_paciente, asunto, cuerpo, estado)
+    values (now(), patient.email, title, body, estado);
 
     return new;
 end;
