6bc937bc9d2fd517d0017a2dc9ac94ca33bb2665
Francisco Ruiz <francisco.ruiz@gmail.com>
2023-06-12 23:16:02 -0300

    fix: remove comments because they generate a file reading bug

--- sql/data/5-agenda.sql
+++ sql/data/5-agenda.sql
@@ -19,16 +19,12 @@ insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, d
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (41244925, 2, 2, '14:00:00', '23:00:00', '0h 30m 0s');
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (20253212, 3, 1, '14:00:00', '23:00:00', '0h 30m 0s');
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (31219475, 4, 4, '12:00:00', '12:30:00', '0h 30m 0s');
--- Medicos que atienden
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (34765129, 1, 5, '15:00:00', '16:00:00', '1h 00m 0s');
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (34765129, 5, 2, '15:25:00', '15:50:00', '0h 25m 0s');
-
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (40271684, 3, 2, '14:00:00', '14:30:00', '0h 30m 0s');
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (40271684, 4, 3, '12:00:00', '13:00:00', '1h 00m 0s');
-
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (25648973, 2, 1, '11:00:00', '11:35:00', '0h 35m 0s');
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (25648973, 4, 1, '14:00:00', '14:45:00', '0h 45m 0s');
--- Medic that attends every day
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (12341234, 1, 6, '09:00:00', '14:00:00', '0h 30m 0s');
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (12341234, 2, 6, '09:00:00', '14:00:00', '0h 30m 0s');
 insert into agenda (dni_medique, dia, nro_consultorio, hora_desde, hora_hasta, duracion_turno) values (12341234, 3, 6, '09:00:00', '14:00:00', '0h 30m 0s');
