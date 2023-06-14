27db8a0f6b2479a824c8aba445ba694fae895423
Francisco Ruiz <francisco.ruiz@gmail.com>
2023-06-13 21:35:44 -0300

    fix: add reserve and available appointment compatibility in cancel_appointment.sql

--- sql/sp/cancel_appointment.sql
+++ sql/sp/cancel_appointment.sql
@@ -5,9 +5,15 @@ declare
 begin
     canceled_appointment_count := 0;
 
-    for result in select t.nro_turno as nro_turno, p.nro_paciente as nombre_paciente, p.apellido as apellido_paciente, p.telefono as telefono_paciente, p.email as email_paciente, m.nombre as nombre_medique, m.apellido as apellido_medique from turno t, paciente p, medique m where p.nro_paciente = t.nro_paciente and m.dni_medique = t.dni_medique and t.dni_medique = dni and t.estado in ('disponible', 'reservado') and t.fecha between date_from and date_to loop
+    for result in select t.nro_turno as nro_turno, t.nro_paciente, p.nombre as nombre_paciente, p.apellido as apellido_paciente, p.telefono as telefono_paciente, p.email as email_paciente, m.nombre as nombre_medique, m.apellido as apellido_medique from turno t, paciente p, medique m where p.nro_paciente = t.nro_paciente and m.dni_medique = t.dni_medique and t.dni_medique = dni and t.estado in ('reservado') and date(t.fecha) between date_from and date_to
+                  union
+                  select t.nro_turno as nro_turno, t.nro_paciente, null as nombre_paciente, null as apellido_paciente, null as telefono_paciente, null as email_paciente, m.nombre as nombre_medique, m.apellido as apellido_medique from turno t, medique m where m.dni_medique = t.dni_medique and t.dni_medique = dni and t.estado in ('disponible') and date(t.fecha) between date_from and date_to loop
         update turno set estado = 'cancelado' where nro_turno = result.nro_turno;
-        insert into reprogramacion (nro_turno, nombre_paciente, apellido_paciente, telefono_paciente, email_paciente, nombre_medique, apellido_medique, estado) values (result.nro_turno, result.nombre_paciente, result.apellido_paciente, result.telefono_paciente, result.email_paciente, result.nombre_medique, result.apellido_medique, 'pendiente');
+
+        if result.nro_paciente is not null then
+            insert into reprogramacion (nro_turno, nombre_paciente, apellido_paciente, telefono_paciente, email_paciente, nombre_medique, apellido_medique, estado) values (result.nro_turno, result.nombre_paciente, result.apellido_paciente, result.telefono_paciente, result.email_paciente, result.nombre_medique, result.apellido_medique, 'pendiente');
+        end if;
+
         canceled_appointment_count := canceled_appointment_count + 1;
     end loop;
 
